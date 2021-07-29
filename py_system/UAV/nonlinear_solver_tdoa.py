ba0520f0f9ee984f1cf448df50d65adcba8628af
pingzhou.ming <mingpz@mail.ustc.edu.cn>
2021-07-29 14:59:06 +0800

    2021.7.29 +2

--- py_system/UAV/nonlinear_solver_tdoa.py
+++ py_system/UAV/nonlinear_solver_tdoa.py
@@ -45,6 +45,7 @@ def equations(p):
 def scipy_solver():
     x_est, y_est = fsolve(equations, (0.0, 0.0))
     print("scipy_solver() results : (%.6f, %.6f)" % (x_est, y_est))
+    return x_est, y_est
 
 def fixedpoint_iterative_solver(total_iters):
     x_est = 0.0
@@ -58,6 +59,7 @@ def fixedpoint_iterative_solver(total_iters):
         if (abs(x_est-x_est_before) < 1E-6) and (abs(y_est-y_est_before) < 1E-6):
             break
     print("Itr %d : fixedpoint_iterative_solver() TDOA results (%.6f, %.6f)" % (itr, x_est, y_est))
+    return x_est, y_est
 
 def newton_iterative_solver(total_iters):
     x_est = 0.0
@@ -74,6 +76,30 @@ def newton_iterative_solver(total_iters):
         y_est += delta_y
         print("Itr %d : (%.6f %.6f) ---> (%.6f %.6f)" % (itr, delta_x, delta_y, x_est, y_est))
     print("Itr %d : newton_iterative_solver() TDOA results (%.6f, %.6f)" % (itr, x_est, y_est))
+    return x_est, y_est
+
+def direct_solver():
+    x = 0.0
+    y = 0.0
+    alpha = _A2 / _A1
+    beta = -(_A2/_A1)*_B1 + _B2
+    A = (_A1**2) * (1 + alpha**2) - 1.0
+    B = (_A1**2) * (2*alpha*beta-2*alpha*_Y0-2*_X0) + 2*_B1
+    C = (_A1**2) * (_X0**2+beta**2+_Y0**2-2*_Y0*beta) - _B1**2
+    print('Middle result is %.6f' % (B*B-4*A*C))
+    x1 = (-B + np.sqrt(B*B-4*A*C))/(2*A)
+    x2 = (-B - np.sqrt(B*B-4*A*C))/(2*A)
+    y1 = alpha * x1 + beta
+    y2 = alpha * x2 + beta
+
+    if (x1 > 0) and (y1 > 0) and (x1 < 20) and (y1 < 20):
+        x = x1
+        y = y1
+    else:
+        x = x2
+        y = y2
+    print("direct_solver() TDOA results (%.6f, %.6f)" % (x, y))
+    return x, y
 
 if __name__ == "__main__":
     print("Unit test")
@@ -81,3 +107,4 @@ if __name__ == "__main__":
     fixedpoint_iterative_solver(100)
     fixedpoint_iterative_solver(1000)
     newton_iterative_solver(10)
+    direct_solver()
